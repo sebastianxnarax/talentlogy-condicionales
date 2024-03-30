b4d27cc2504cbe55ae207dd9b05646d474f60036
sebastian <sebastian1_2_@hotmail.com>
2024-03-29 20:33:08 -0500

    segundo

--- respuestas.js
+++ respuestas.js
@@ -20,14 +20,40 @@ function respuesta2() {
     alert("El número es impar."); // Si el residuo de la división por 2 no es 0, el número es impar
   }
 }
-//respuesta 3 
+//respuesta 3
 function respuesta3() {
-    numero1= prompt ("digite el primer numero");
-    numero2= prompt ("digite el segundo numero");
-    if (numero1 % numero2 === 0 || numero2 % numero1 === 0) {
-        alert("los numeros son multiplos"); // Si alguno de los números es divisible por el otro sin residuo, son múltiplos entre sí
-    } else {
-        alert("los numeros NO son multiplos"); // Si ninguno de los números es divisible por el otro sin residuo, no son múltiplos entre sí
-    }
-    
+  numero1 = prompt("digite el primer numero");
+  numero2 = prompt("digite el segundo numero");
+  if (numero1 % numero2 === 0 || numero2 % numero1 === 0) {
+    alert("los numeros son multiplos"); // Si alguno de los números es divisible por el otro sin residuo, son múltiplos entre sí
+  } else {
+    alert("los numeros NO son multiplos"); // Si ninguno de los números es divisible por el otro sin residuo, no son múltiplos entre sí
+  }
+}
+//respuesta 4
+function respuesta4() {
+  n1 = parseInt(prompt("digite el primer numero"));
+  n2 = parseInt(prompt("digite el segundo numero"));
+  if (n1 > n2) {
+    alert("el numero " + n1 + " es mayor");
+  } else if (n2 > n1) {
+    alert("el numero " + n2 + " es mayor");
+  } else {
+    alert("los dos numeros son iguales");
+  }
+}
+//respuesta 5
+function respuesta5() {
+  calificacion = parseInt(prompt("digite la nota"));
+  if (calificacion < 0 || calificacion > 10) {
+    alert ("Calificación inválida");
+  } else if (calificacion < 5) {
+    alert ("Insuficiente");
+  } else if (calificacion >= 5 && calificacion < 7) {
+    alert ("Suficiente");
+  } else if (calificacion >= 7 && calificacion < 9) {
+    alert ("Bien");
+  } else {
+    alert ("Excelente");
+  }
 }
